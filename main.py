faca297fdde81ddc9e2e6f58e93778d111d7f870
Armaan Sengupta <asengupt@uwaterloo.ca>
2023-03-23 19:42:00 -0400

    Lines

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("hello world")
+import math
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y   
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Point(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return Point(self.x / other, self.y / other)
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def toString(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+
+class Line():
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.angle = self.get_angle();
+
+    def get_length(self):
+        return math.sqrt((self.p2.x - self.p1.x)**2 + (self.p2.y - self.p1.y)**2)
+    
+    def get_angle(self):
+        return math.atan2(self.p2.y - self.p1.y, self.p2.x - self.p1.x)
+
+    def set_length(self, length):
+        self.p2.x = self.p1.x + length * self.get_slope()
+        self.p2.y = self.p1.y + length
+
+    #make a function to update point 2 based on the angle and length
+    def set_angle(self, angle):
+        self.p2.x = self.p1.x + self.get_length() * math.cos(angle)
+        self.p2.y = self.p1.y + self.get_length() * math.sin(angle)
+
+    def toString(self):
+        return "Line: " + self.p1.toString() + " to " + self.p2.toString()
---
+++ visualization.py
@@ -0,0 +1,75 @@
+from main import *
+
+WIDTH=800  
+HEIGHT=800
+SCALE_FACTOR=200
+
+ORIGIN_OFFSET=WIDTH/2
+
+#using the imported main.py use the line class to create a line class to be drawn in pygame
+class Line(Line):
+    #this is the constructor for the line class
+    def __init__(self, p1, p2):
+        #call the parent constructor
+
+        super().__init__(p1 , p2)
+        print(self.toString())
+        #set the color to be drawn
+        self.color = (0,0,0)
+        #set the width of the line
+        self.width = 1
+
+    #this function will draw the line to the screen
+    def draw(self):
+        pygame.draw.line(screen, self.color, (self.p1.x * SCALE_FACTOR + ORIGIN_OFFSET, self.p1.y * -SCALE_FACTOR + ORIGIN_OFFSET), (self.p2.x * SCALE_FACTOR + ORIGIN_OFFSET, self.p2.y * -SCALE_FACTOR + ORIGIN_OFFSET), self.width)
+
+        #draw circle at the start of the line
+        pygame.draw.circle(screen, (255,0,0), (self.p1.x * SCALE_FACTOR + ORIGIN_OFFSET, self.p1.y * SCALE_FACTOR + ORIGIN_OFFSET), 5)
+        
+        
+        
+#list of all currentely displayed lines
+lines = []    
+
+#our special points
+p1 = Point(0.75,0.1)
+p2 = Point(0.5,0.5)
+p3 = Point(0.2,0.6)  
+
+#our special lines
+l1 = Line(Point(0, 0), Point(1, 1))
+l1.set_angle(90)
+
+lines.append(l1)
+
+# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+    
+    for line in lines:
+        line.draw()
+    # Draw a solid blue circle in the center
+    
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
